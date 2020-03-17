c48e13028607454ca63436d69a63296f38de9f9c
XiangweiChen <xchen239@buffalo.edu>
2020-03-17 18:47:34 -0400

    add UI to login page #22

---
+++ app/src/main/java/com/example/myfirstapp/MainActivity.java
@@ -0,0 +1,26 @@
+package com.example.myfirstapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button loginBtn = (Button)findViewById(R.id.loginBtn);
+        loginBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent startIntent = new Intent(getApplicationContext(), Main2Activity.class); //Main2Activity is home page
+                startActivity(startIntent);
+            }
+        });
+    }
+}
